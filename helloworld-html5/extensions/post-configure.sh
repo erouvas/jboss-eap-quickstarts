4302d599dc1b4a7a0279fe34c8d5eeb407d46c37
Efstathios Rouvas <erouvas@redhat.com>
2022-09-12 03:27:53 +0100

    wip

--- helloworld-html5/extensions/post-configure.sh
+++ helloworld-html5/extensions/post-configure.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Executing patch.cli"
+echo "Executing patch.cli" > /tmp/post-configure-out.txt
 $JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/system-properties.cli
