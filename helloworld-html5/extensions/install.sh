164ebbaa4586c521151146ba956d95f821f5efb5
Efstathios Rouvas <erouvas@redhat.com>
2022-09-12 03:58:20 +0100

    wip

--- helloworld-html5/extensions/install.sh
+++ helloworld-html5/extensions/install.sh
@@ -16,3 +16,6 @@ injected_dir=$1
 source /usr/local/s2i/install-common.sh
 install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
+
+# copy any needed files into the target build.
+cp -rf ${injected_dir} $JBOSS_HOME/extensions
